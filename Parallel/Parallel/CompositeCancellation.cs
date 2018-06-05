36f40f8c35b8ef15d4c890c7d758e8c0be79f7fd
Kevin Park <hyunbin7303@gmail.com>
2018-06-05 04:57:46 -0400

    Commit new one

---
+++ Parallel/Parallel/CompositeCancellation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallel
+{
+    public class CompositeCancellation
+    {
+        public static void Main()
+        {
+            // Object responsible for creating cancellation token and sending cancellation request to 
+            //all copies of that token.
+            CancellationTokenSource planned = new CancellationTokenSource();
+            var preventative = new CancellationTokenSource();
+            var Emergency = new CancellationTokenSource();
+            var paranoid = CancellationTokenSource.CreateLinkedTokenSource(
+                planned.Token, preventative.Token, Emergency.Token);
+
+
+            Task.Factory.StartNew(() =>
+            {
+                int i = 0;
+                while(true)
+                {
+                    paranoid.Token.ThrowIfCancellationRequested();
+                    Console.WriteLine($"{i++}\t");
+                    Thread.Sleep(1000);
+                }
+            }, paranoid.Token);
+            // Essentially what this implies is I can now request cancellation on either planned or preventative or Emergency token sources.
+            // All of this possible because we have a token from a linked token source that will stop our execution so it doesn't matter
+            // what I do.
+
+            // It does not matter whether it is preventative or planned or emergency.    
+            Console.ReadKey();
+            Emergency.Cancel();
+
+
+            Console.WriteLine("Main Program Done.   ");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Parallel/Parallel/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel
+{
+    internal static class Program
+    { 
+        public static void Write(char c)
+        {
+            int i = 1000;
+            while (i --> 0)
+            {
+                Console.Write(c);
+            }
+        }
+
+        public static void Write(object o)
+        {
+            int i = 1000;
+            while(i --> 0)
+            {
+                Console.Write(o);
+            }
+        }
+
+
+        public static int TextLength(object o)
+        {
+            Console.WriteLine($"\nTask with id {Task.CurrentId} processing object {o}... ");
+            return o.ToString().Length;
+        }
+
+        public static void Main()
+        {
+            //Task task = new Task(Write, "Hi");
+            //task.Start();
+            //Task.Factory.StartNew(Write, 123);
+
+            string text1 = "Testing", text2 = "haha";
+            var task1 = new Task<int>(TextLength, text1);
+            task1.Start();
+            Task<int> task2 = Task.Factory.StartNew(TextLength, text2);
+            Console.WriteLine($"Length of '{text1}' is {task1.Result}");
+            Console.WriteLine($"Length of '{text2}' is {task2.Result}");
+            Console.WriteLine("Main Program. ");
+            Console.ReadKey();
+        }
+    }
+}
+// Tasks get executed on separate threads which means at some point you might want to cancel a particular task,
+// and task parallize provides explicit mechanism for doing exactly that . 
